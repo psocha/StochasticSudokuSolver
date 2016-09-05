ebe3788a7306d864dff909ff4b90a53b80840d8d
Peter Socha <psocha@uwaterloo.ca>
2016-09-05 10:46:19 -0700

    Partial main function and dynamic UI

--- solver.js
+++ solver.js
@@ -57,13 +57,80 @@ document.addEventListener('DOMContentLoaded', function() {
     var button = document.createElement('button');
     button.classList.add('sss-button');
     button.innerHTML = 'Solve';
+    button.addEventListener('click', main);
     gridBox.appendChild(button);
 
 });
 
+// Function called in response to a button click
+function main() {
+    setEnabled(false);
+    var grid = [];
+    for (var row = 0; row < 9; row++) {
+        grid[row] = [];
+        for (var column = 0; column < 9; column++) {
+            var cellValue = document.getElementById('box_' + row.toString() + column.toString()).value;
+            if (cellValue == '') {
+                grid[row][column] = 0;
+            } else {
+                grid[row][column] = cellValue;
+            }
+        }
+    }
+    var contradictions = numContradictions(grid);
+    if (contradictions > 0) {
+        displayError('This sudoku puzzle is unsolvable.');
+        setEnabled(true);
+        return;
+    }
+
+    setEnabled(true);
+}
+
+function setEnabled(setting) {
+    for (var row = 0; row < 9; row++) {
+        for (var column = 0; column < 9; column++) {
+            document.getElementById('box_' + row.toString() + column.toString()).disabled = !setting;
+        }
+    }
+    var button = document.getElementsByClassName('sss-button')[0];
+    button.disabled = !setting;
+}
+
+// Display the current state of the sudoku grid on the page.
+function updateGrid(grid, errors) {
+    for (var row = 0; row < 9; row++) {
+        for (var column = 0; column < 9; column++) {
+            var cell = document.getElementById('box_' + row.toString() + column.toString());
+            cell.value = grid[row][column];
+        }
+    }
+    var label = document.getElementsByClassName('sss-label')[0];
+    label.classList.remove('sss-green');
+    label.classList.remove('sss-red');
+    label.innerHTML = 'Errors on grid: ' + errors.toString();
+}
+
+// Notify the user of an unsolvable grid.
+function displayError(error) {
+    var label = document.getElementsByClassName('sss-label')[0];
+    label.classList.remove('sss-green');
+    label.classList.add('sss-red');
+    label.innerHTML = error;
+}
+
+// Notify the user of a successful run.
+function displaySuccess(swaps) {
+        var label = document.getElementsByClassName('sss-label')[0];
+        label.classList.remove('sss-red');
+        label.classList.add('sss-green');
+        label.innerHTML = 'Puzzle solved after ' + swaps.toString() + ' swaps.';
+}
+
+// Calculate the number of errors in a partial or full grid.
 function numContradictions(grid) {
     var contradictions = 0;
-    var occurrences;
+    var occurrences = [];
 
     // All rows
     for (var row = 0; row < 9; row++) {
